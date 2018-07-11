e59d7e614b051fa97fd049602dd0f42702dff1c7
Ivo Petkov <ivo@ivopetkov.com>
2018-07-11 14:21:54 +0300

    Updated comments.

--- ide-helper.php
+++ ide-helper.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * Users addon for Bear Framework
+ * https://github.com/ivopetkov/users-bearframework-addon
+ * Copyright (c) Ivo Petkov
+ * Free to use under the MIT license.
+ */
+
 namespace BearFramework {
 
     /**
